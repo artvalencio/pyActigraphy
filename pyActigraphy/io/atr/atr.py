b70dacac74848bacb9734242378f1a10fb01538d
Arthur Valencio <33551656+artvalencio@users.noreply.github.com>
2023-11-24 18:14:16 -0300

    fix first line reading according to actlumus

--- pyActigraphy/io/atr/atr.py
+++ pyActigraphy/io/atr/atr.py
@@ -45,11 +45,13 @@ class RawATR(BaseRaw):
         header = {}
         with open(input_fname) as fp:
             first_line = fp.readline()
-            #if not re.match(r"\+-*\+ \w+ \w+ \w+ \+-*\+", first_line):
-            #    raise ValueError(
-            #        "The input file ({}) does not ".format(input_fname)
-            #        + "seem to contain the usual header.\n Aborting."
-            #    )
+            if re.match(r"\#\w*=\d.\d.\d",first_line):
+                first_line = fp.readline()
+            if not re.match(r"\+-*\+ \w+ \w+ \w+ \+-*\+", first_line):
+                raise ValueError(
+                    "The input file ({}) does not ".format(input_fname)
+                    + "seem to contain the usual header.\n Aborting."
+                )
             for line in fp:
                 if '+-------------------' in line:
                     break
